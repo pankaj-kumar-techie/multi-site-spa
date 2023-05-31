1d8217dd1e0e082dbc7e9df448a90a9ac4f932fe
pahariyatri <pahariyatri@gmail.com>
2023-05-31 15:36:55 +0530

    update api signature

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,6 +1,6 @@
 export class Config {
     // static SPA_SERVICE_BASE_URL = "http://localhost:8090/";
-    static SPA_SERVICE_BASE_URL = "http://3.15.11.80/";
+    static SPA_SERVICE_BASE_URL = "https://3.15.11.80/";
     static BLOG_SERVICE_BASE_URL = "http://localhost:8091/";
     static IS_LOGGED_IN = "isLoggedIn";
     static ACCESS_TOKEN = "accessToken";
