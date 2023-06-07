0fc611a71b6262d8c18672bbe1eab9c640cdcb57
pahariyatri <pahariyatri@gmail.com>
2023-06-07 22:20:22 +0530

    added header styles

--- src/components/layouts/header/v1/FirstHeader.tsx
+++ src/components/layouts/header/v1/FirstHeader.tsx
@@ -1,8 +1,87 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export default function FirstHeader(props: { data: any }) {
+    const [headerData, setHeaderData] = useState<any>([])
+    const classic = {
+        section: "text-gray-600 body-font",
+        backgroundColor: "bg-orange-200",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-gray-600 text-lg mb-6",
+        imageWrapper: "hidden",
+        bodyWrapper: "sm:w-1/2 sm:pl-16",
+        aboutImage: "https://stackoverflow.co/img/product/teams/teams-integrations-spot.svg",
+        aboutImageWrapper: "w-80 h-80  object-cover",
+    };
+    const modern = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+        imageWrapper: "absolute top-[-550px] z-0    left-[50%] h-[1500px] translate-x-[-50%]",
+        bodyWrapper: "sm:w-1/2 z-[9] sm:pl-16",
+        aboutImage: "	https://stackoverflow.co/img/product/home/bg-header-mobile.png",
+        aboutImageWrapper: "w-80 z-50 h-80 rounded-full object-cover",
+
+    };
+    const plain = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+    const stack_left = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+    const stack_center = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+    const stack_right = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+    const fixed = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+    const transparent = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+    const split = {
+        section: "text-gray-600 body-font  relative overflow-hidden",
+        backgroundColor: "bg-gray-900",
+        title: "text-3xl font-extrabold text-white sm:text-4xl mb-6",
+        subTitle: "text-white text-lg mb-6",
+    };
+
+
+    const [headerTheme, setHeaderTheme] = useState<string>("")
+
+    const styles = {
+        Theme1: classic,
+        Theme2: modern,
+        Theme3: plain,
+    }[headerTheme] || classic;
+
+    useEffect(() => {
+        setHeaderData(props.data);
+        setHeaderTheme(props.data.theme || "Theme1")
+    }, [props.data])
+
     return (
-        <header>
+        <header className={styles.section}>
             <nav
                 className="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
                 data-te-navbar-ref="">
