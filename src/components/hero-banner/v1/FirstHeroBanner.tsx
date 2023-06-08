999a2a8c8f58331ea460db8efd6daac6f66bb2b5
Uday <bhanusharm122@gmail.com>
2023-06-09 01:03:21 +0530

    added new styles at v1

--- src/components/hero-banner/v1/FirstHeroBanner.tsx
+++ src/components/hero-banner/v1/FirstHeroBanner.tsx
@@ -1,69 +1,70 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const styles1 = {
-    section: "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  min-h-screen flex flex-col justify-center",
-    title: "lg:text-[8rem] md:text-[7rem] text-[2.5rem] font-bold text-purssian-blue text-center",
-    subTitle: "mt-3 max-w-md mx-auto text-xl text-gray-300 sm:text-2xl md:mt-5 md:text-[2.5rem] md:max-w-3xl text-center",
-    buttonWrapper: "mt-10 sm:flex justify-center",
-    button: "w-[250px]  flex items-center justify-center px-6  border border-transparent text-base font-medium rounded-md text-white bg-indigo-600  md:text-lg md:px-8",
-    buttonSecondary: "mt-3  sm:w-[250px] rounded-md  shadow sm:mt-0  sm:ml-3",
-    buttonSecondaryLink: "w-[250px]  flex  items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-gradient-to-r from-white to-white hover:text-white hover:from-pink-500 hover:to-yellow-500 md:py-4 md:text-lg md:px-10"
+  section: "bg-custom-brown  min-h-screen flex flex-col justify-center",
+  title:
+    "lg:text-[8rem] md:text-[7rem] text-[2.5rem] font-bold text-purssian-blue text-center",
+  subTitle:
+    "mt-3 max-w-md  mx-auto text-3xl text-custom-black  sm:text-4xl md:mt-5 md:text-[2.5rem] md:max-w-3xl text-center",
+  buttonWrapper: "mt-10 sm:flex justify-center",
+  button:
+    "  flex items-center justify-center   border border-transparent text-base font-medium rounded-md text-white bg-indigo-600  md:text-lg ",
+  buttonSecondary: "mt-3  sm:w-[250px] rounded-md  shadow sm:mt-0  sm:ml-3",
+  buttonSecondaryLink:
+    "w-[250px]  flex  items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-white hover:text-white hover:from-pink-500 hover:bg-custom-blue md:py-4 md:text-lg md:px-10",
 };
 
 const styles2 = {
-    section: "bg-gray-100 min-h-screen flex flex-col justify-center",
-    title: "text-4xl font-bold text-gray-800 text-center",
-    subTitle: "mt-3 max-w-md mx-auto text-lg text-gray-600 sm:text-xl md:mt-5 md:text-2xl md:max-w-3xl text-center",
-    buttonWrapper: "mt-10 flex justify-center",
-    button: "px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-lg md:px-6",
-    buttonSecondary: "mt-3 rounded-md shadow sm:mt-0 sm:ml-3",
-    buttonSecondaryLink: "px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-white hover:text-white hover:bg-indigo-700 md:py-4 md:text-lg md:px-10",
+  section: "bg-gray-100 min-h-screen flex flex-col justify-center",
+  title: "text-4xl font-bold text-gray-800 text-center",
+  subTitle:
+    "mt-3 max-w-md mx-auto text-lg text-gray-600 sm:text-xl md:mt-5 md:text-2xl md:max-w-3xl text-center",
+  buttonWrapper: "mt-10 flex justify-center",
+  button:
+    "px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:text-lg md:px-6",
+  buttonSecondary: "mt-3 rounded-md shadow sm:mt-0 sm:ml-3",
+  buttonSecondaryLink:
+    "px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-white hover:text-white hover:bg-indigo-700 md:py-4 md:text-lg md:px-10",
 };
 export default function FirstHeroBanner(props: { data: any }) {
-    const [bannerData, setBannerData] = useState<any>([])
-    const [bannerTheme, setBannerTheme] = useState<string>("")
-    const styles = {
-        Theme1: styles1,
-        Theme2: styles2,
+  const [bannerData, setBannerData] = useState<any>([]);
+  const [bannerTheme, setBannerTheme] = useState<string>("");
+  const styles =
+    {
+      Theme1: styles1,
+      Theme2: styles2,
     }[bannerTheme] || styles1;
-    useEffect(() => {
-        setBannerData(props.data);
-        setBannerTheme(props.data.theme || "Theme1")
-    }, [props.data])
+  useEffect(() => {
+    setBannerData(props.data);
+    setBannerTheme(props.data.theme || "Theme1");
+  }, [props.data]);
 
-    return (
-        <section className={styles.section}>
-            <div className="py-12 flex flex-col justify-center items-center">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="text-right">
-                        <h1 className={styles.title}>
-                            {bannerData.title}
-                        </h1>
-                        <p className={styles.subTitle}>
-                            {bannerData.subTitle}
-                        </p>
+  return (
+    <section className={styles.section}>
+      <div className="py-12 flex flex-col justify-center items-center">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-right">
+            <h1 className={styles.title}>{bannerData.title}</h1>
+            <p className={styles.subTitle}>{bannerData.subTitle}</p>
 
-                        <div className={styles.buttonWrapper}>
-                            <div className={styles.button}>
-                                <a
-                                    href="/echo"
-                                    className="w-[200px] flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                                >
-                                    Get started
-                                </a>
-                            </div>
-                            <div className={styles.buttonSecondary}>
-                                <a
-                                    href="/echo"
-                                    className={styles.buttonSecondaryLink}
-                                >
-                                    Learn more
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className={styles.buttonWrapper}>
+              <div className={styles.button}>
+                <a
+                  href="/echo"
+                  className="w-[250px] flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purssian-blue hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                >
+                  Get started
+                </a>
+              </div>
+              <div className={styles.buttonSecondary}>
+                <a href="/echo" className={styles.buttonSecondaryLink}>
+                  Learn more
+                </a>
+              </div>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         "custom-blue": "#328CC1",
         "custom-gold": "#D9B310",
         "custom-brown":"#f8f5f0",
-        "purssian-blue":"#0B3C5D"
+        "purssian-blue":"#0B3C5D",
+        "custom-black":"#1D2731",
       },
     },
   },
