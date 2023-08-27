8339e3cf48c5d73c635bc878c0e5fe9e24e910a6
Pankaj Kumar <pankajkumar.techie@gmail.com>
2023-08-27 15:39:34 +0530

    modified:   src/@local-db/northVibeDesign.tsx 	modified:   src/components/layouts/header/v1/FirstHeader.tsx

--- src/@local-db/northVibeDesign.tsx
+++ src/@local-db/northVibeDesign.tsx
@@ -15,7 +15,7 @@ export const northVibeDesign: Section[] = [
     data: {
       type: "Header",
       id: "001",
-      title: "North Vibe Design",
+      title: "NV Design",
       subTitle: "",
       description: "",
       version: "v1",
@@ -53,7 +53,7 @@ export const northVibeDesign: Section[] = [
       images: [
         {
           id: '1',
-          imageSrc: 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+          imageSrc: 'https://plus.unsplash.com/premium_photo-1663956108056-66e355e5de96?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80',
           imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
         },
       ],
