c3b4d5044b52134d2ed838d9f8753a146aebfff9
Pankaj Kumar <pankajkumar.techie@gmail.com>
2023-08-29 13:50:57 +0530

    modified:   src/components/layouts/footer/v1/FirstFooter.tsx 	modified:   src/components/layouts/header/v1/FirstHeader.tsx 	modified:   src/components/layouts/header/v1/SecondHeader.tsx 	modified:   src/views/hero-banner/v1/FirstHeroBanner.tsx

--- src/components/layouts/header/v1/SecondHeader.tsx
+++ src/components/layouts/header/v1/SecondHeader.tsx
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import {ThemeContext} from "../../../../themes/ThemeProvider";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../../../../themes/ThemeProvider";
 
 export default function FirstHeader(props: { data: any }) {
     const { theme } = useContext(ThemeContext);
@@ -37,13 +37,13 @@ export default function FirstHeader(props: { data: any }) {
     return (
         <>
 
-            <div style={{backgroundColor: `${color}`}} className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+            <div style={{ backgroundColor: `${color}` }} className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
                 <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
                     < Link to='/'>
-                        <h1 style={{color: `${textColor}`}}
+                        <h1 style={{ color: `${textColor}` }}
                             className='font-bold text-2xl lg:text-3xl '>{headerData.title ?? "Site Name"}</h1>
                     </Link>
-                    <ul style={{color: `${textColor}`}} className='hidden sm:flex'>
+                    <ul style={{ color: `${textColor}` }} className='hidden sm:flex'>
                         <li className='p-4'>
                             < Link to='/'>Home</Link>
                         </li>
@@ -60,14 +60,14 @@ export default function FirstHeader(props: { data: any }) {
 
                     {/* Mobile Button*/}
                     <div onClick={handleNav} className='block sm:hidden z-10'>
-                        {nav ? <svg stroke="currentColor" style={{color: `${textColor}`}} fill="currentColor"
-                                    stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em"
-                                    xmlns="http://www.w3.org/2000/svg">
+                        {nav ? <svg stroke="currentColor" style={{ color: `${textColor}` }} fill="currentColor"
+                            stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path>
-                        </svg> : <svg style={{color: `${textColor}`}} stroke="currentColor" fill="currentColor"
-                                      stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em"
-                                      xmlns="http://www.w3.org/2000/svg">
+                        </svg> : <svg style={{ color: `${textColor}` }} stroke="currentColor" fill="currentColor"
+                            stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em"
+                            xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0 624H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zm0-312H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"></path>
                         </svg>}
