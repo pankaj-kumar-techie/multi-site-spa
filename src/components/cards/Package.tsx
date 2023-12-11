ed1a2aee98da7af6c0cd91a28d000c041dc1258b
Uday Bhanu <bhanusharm122@gmail.com>
2023-12-11 21:49:09 +0530

    ui

--- src/components/cards/Package.tsx
+++ src/components/cards/Package.tsx
@@ -121,7 +121,7 @@ export default function PackageCard(packageDetail: Package) {
             <span className="mr-2">View Trek</span> <FaChevronCircleRight />
           </button>
         </div>
-        <div className="flex bg-zinc-400 rounded-b-lg justify-center p-2">
+        {/* <div className="flex bg-zinc-400 rounded-b-lg justify-center p-2">
           <div className="flex items-center">
             <svg
               className="w-6 h-6 text-yellow-300 ms-1"
@@ -169,7 +169,7 @@ export default function PackageCard(packageDetail: Package) {
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
           </div>
-        </div>
+        </div> */}
       </a>
     </div>
   );
