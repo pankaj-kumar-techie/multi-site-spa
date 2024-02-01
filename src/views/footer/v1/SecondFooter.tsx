3436ad99dc711493b96955b8d7898743b1fa71cb
Pankaj Kumar <pankajkumar.techie@gmail.com>
2024-02-01 17:42:20 +0530

    modified:   src/@local-db/pahariyatri.tsx 	modified:   src/modal/Section.ts 	new file:   src/utils/IconUtils.tsx 	modified:   src/views/footer/v1/SecondFooter.tsx

--- src/views/footer/v1/SecondFooter.tsx
+++ src/views/footer/v1/SecondFooter.tsx
@@ -1,13 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-import { FaFacebook, FaInstagram, FaLinkedin, } from "react-icons/fa";
-import { TiSocialPinterest } from "react-icons/ti"
 import { ThemeContext } from "../../../themes/ThemeProvider";
+import Icon from "../../../utils/IconUtils";
+import { SocialLink } from "../../../modal/Section";
 
 export default function SecondFooter(props: { data: any }) {
     const { theme } = useContext(ThemeContext);
     const [footerData, setFooterData] = useState<any>({
         title: "",
         description: "",
+        socialLink: [],
     })
     useEffect(() => {
         setFooterData(props.data.footer);
@@ -18,12 +19,20 @@ export default function SecondFooter(props: { data: any }) {
                 <div
                     className="flex flex-col max-w-[1400px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500 ">
                     <p className={`${theme.colors.secondary} py-4 font-medium  hover:${theme.colors.secondary} hover:ease-in-out font-jakarta duration-200 cursor-pointer`}>
-                         { footerData.copyright || "Made With ❤️ by Loonds"}</p>
+                        Made With ❤️ by {footerData.copyright || " Loonds"}</p>
                     <div className="flex  justify-between sm:w-[300px] pt-4 text-2xl">
-                        <FaFacebook className="cursor-pointer hover:text-white hover:ease-in-out duration-300" />
-                        <FaInstagram className="cursor-pointer hover:text-white hover:ease-in-out duration-300" />
-                        <FaLinkedin className="cursor-pointer hover:text-white hover:ease-in-out duration-300" />
-                        <TiSocialPinterest className="cursor-pointer  hover:text-white hover:ease-in-out duration-300" size={30} />
+
+
+                        {footerData.socialLink.map((socialIcon: SocialLink) => (
+                            <Icon
+                                key={socialIcon.id}
+                                id={socialIcon.id}
+                                name={socialIcon.name}
+                                size={30}
+                                className="cursor-pointer hover:text-white hover:ease-in-out duration-300"
+                                link={socialIcon.link}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
